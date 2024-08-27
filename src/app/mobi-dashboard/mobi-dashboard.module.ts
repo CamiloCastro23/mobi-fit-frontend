1428233bc88db19f02cacf61799fc3efda9d12bb
CamiloCastro23 <camilo.castro2996@gmail.com>
2024-08-26 21:19:57 -0500

    add mobi-dashboard module with --routing

---
+++ src/app/mobi-dashboard/mobi-dashboard.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MobiDashboardRoutingModule } from './mobi-dashboard-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MobiDashboardRoutingModule
+  ]
+})
+export class MobiDashboardModule { }

0aa77530bf58299b8fa6f1fcdb9cfeba35c461e8
CamiloCastro23 <camilo.castro2996@gmail.com>
2024-08-26 22:00:23 -0500

    adding dashboard pages

--- src/app/mobi-dashboard/mobi-dashboard.module.ts
+++ src/app/mobi-dashboard/mobi-dashboard.module.ts
@@ -2,10 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MobiDashboardRoutingModule } from './mobi-dashboard-routing.module';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { UserPageComponent } from './pages/user-page/user-page.component';
+import { DietPageComponent } from './pages/diet-page/diet-page.component';
+import { WorkoutPageComponent } from './pages/workout-page/workout-page.component';
+import { TutorialPageComponent } from './pages/tutorial-page/tutorial-page.component';
+import { SupportPageComponent } from './pages/support-page/support-page.component';
+import { SettingsPageComponent } from './pages/settings-page/settings-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LayoutPageComponent,
+    HomePageComponent,
+    UserPageComponent,
+    DietPageComponent,
+    WorkoutPageComponent,
+    TutorialPageComponent,
+    SupportPageComponent,
+    SettingsPageComponent
+  ],
   imports: [
     CommonModule,
     MobiDashboardRoutingModule
---
+++ src/app/mobi-dashboard/pages/tutorial-page/tutorial-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tutorial-page',
+  templateUrl: './tutorial-page.component.html',
+  styleUrl: './tutorial-page.component.css'
+})
+export class TutorialPageComponent {
+
+}

afaad172afb16f2631701bf40c91f3567160e837
CamiloCastro23 <camilo.castro2996@gmail.com>
2024-08-27 19:39:15 -0500

    obtener tokens

--- src/app/mobi-dashboard/pages/home-page/home-page.component.ts
+++ src/app/mobi-dashboard/pages/home-page/home-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AuthCognitoService } from '../../services/auth-cognito.service';
 
 @Component({
   selector: 'app-home-page',
@@ -7,13 +8,30 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private authService: AuthCognitoService
+  ) { }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe(params => {
       const code = params['code'];
       if (code) {
-        console.log('Code Captured:', code);
+        this.authService.getToken(code).subscribe({
+          next: (response) => {
+            const accessToken = response.access_token;
+            const idToken = response.id_token;
+            const refreshToken = response.refresh_token;
+
+            console.log('Access Token:', accessToken);
+            console.log('ID Token:', idToken);
+            console.log('Refresh Token:', refreshToken);
+          },
+          error: (error) => {
+            console.error('Error al obtener los tokens:', error);
+          }
+        });
       }
     });
   }
